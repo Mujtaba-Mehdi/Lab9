be62b6117adddae749506175f9a1d3b0d1a0dc12
mujta <mujta@DESKTOP-NQ7KR5A>
2021-10-27 15:04:16 -0600

    Fixed context.xml

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -7,10 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author mujta
- */
+
 public class UserServlet extends HttpServlet {
 
     @Override
